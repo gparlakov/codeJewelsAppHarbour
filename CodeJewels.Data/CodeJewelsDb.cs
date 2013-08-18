c700c4faef1fc8ef75ead60d2b031ce12e7c66b0
gparlakov <gparlakov@gmail.com>
2013-08-18 07:09:39 -0700

    First upload of working code - test it anyway

---
+++ CodeJewels.Data/CodeJewelsDb.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using CodeJewels.Models;
+
+namespace CodeJewels.Data
+{
+    public class CodeJewelsDb : DbContext
+    {
+        public CodeJewelsDb()
+            :base("CodeJewelsDb")
+        {
+            InitializeCloudConnectionString();
+        }
+  
+        private void InitializeCloudConnectionString()
+        {
+            var connectionString = ConfigurationManager.AppSettings["CodeJewelsConnectionString"];
+            if (connectionString != null)
+            {
+                this.Database.Connection.ConnectionString = connectionString;
+            }
+        }
+
+        public DbSet<CodeJewel> CodeJewels { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Vote> Votes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CodeJewel>().HasKey(c => c.Id);
+            modelBuilder.Entity<CodeJewel>().Property(c => c.AuthorEMail).IsRequired().HasMaxLength(250).IsVariableLength();
+            modelBuilder.Entity<CodeJewel>().Property(c => c.Code).IsRequired().HasMaxLength(500).IsVariableLength();
+
+            modelBuilder.Entity<Vote>().HasKey(v => v.Id).Property(v => v.IsUpVote).IsRequired();
+            modelBuilder.Entity<Vote>().Property(v => v.Author).IsRequired().HasMaxLength(250).IsVariableLength();
+
+            modelBuilder.Entity<Category>().HasKey(c => c.Id);
+            modelBuilder.Entity<Category>().Property(c => c.Name).IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
